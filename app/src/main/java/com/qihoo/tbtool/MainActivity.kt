ff71a3fe49b7fa0e4eabe7149e1169fda840b8fc
wenyingzhi <97561745@qq.com>
2020-02-07 15:41:19 +0800

    XP

--- app/src/main/java/com/qihoo/tbtool/MainActivity.kt
+++ app/src/main/java/com/qihoo/tbtool/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/qihoo/tbtool/core/XpInit.kt
@@ -0,0 +1,10 @@
+package com.qihoo.tbtool.core
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class XpInit : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+
+    }
+}
